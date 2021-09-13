c14caccfaa30d7a17f87018fe630008b261f1454
FFO96 <ffoliveres@gmail.com>
2021-09-14 00:08:48 +0200

    Added a dictionary and main.go (with the functions to be done)

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/FFO96/wordchains
+
+go 1.17
---
+++ src/main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+
+}
+
+// TODO: Returns the neighbours of a word
+func createNeighbours() {}
+
+//TODO: Returns the solution if exists
+func getSolutionList() {}
+
+//TODO: Find the path of the solution if exists
+func findPath() {}
